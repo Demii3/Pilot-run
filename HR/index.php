958c014b5caf536b14e83b58eb43d4e28f2d5498
justinsaiii <garcia.justinsimon.09172004@gmail.com>
2026-04-06 17:36:28 +0800

    Update index.php

--- HR/index.php
+++ HR/index.php
@@ -19,7 +19,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>HR Dashboard2ejlrfb24jlrl23rhbjf</title>
+        <title>HR Dashboard</title>
 
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
