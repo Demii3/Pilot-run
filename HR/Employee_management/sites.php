e42d246d99cc4b043768846785e18c79709fcb7c
romsiii <168201792+romsiii@users.noreply.github.com>
2026-04-11 14:13:58 +0800

    inadjust onti sa site ni alexis

--- HR/Employee_management/sites.php
+++
@@ -1,5 +0,0 @@
-<div class="card">
-  <h2>Manage Sites</h2>
-  <p>Add, edit, and delete project sites.</p>
-  <button type="button" class="btn btn-primary" onclick="window.location.href='geofence/sites.php'">Open Manage Sites</button>
-</div>
